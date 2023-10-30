d446ca5394254affb7e4c688858c9967e36dc308
Valentina <valentinasergssvna@gmail.com>
2023-10-30 22:33:41 +0200

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ThemeProvider theme={theme}>
-        <App />
-        </ThemeProvider>
-      </PersistGate>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
